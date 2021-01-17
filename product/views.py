0e45201f2430b63c9a1b90c55223c0d9d7a5b3c5
Daniil <d.palkin94@gmail.com>
2021-01-17 11:49:12 +0600

    added todo_list in test page

--- product/views.py
+++ product/views.py
@@ -1,14 +1,10 @@
 from django.shortcuts import render
-
+from .models import ToDo
 # Create your views here.
 
 
 def homepage(request):
     return render(request, "index.html")
-
-
-def test(request):
-    return render(request, "test.html")
 
 
 def add(request):
@@ -21,3 +17,8 @@ def change(request):
 
 def delete(request):
     return render(request, "delete.html")
+
+
+def test(request):
+    todo_list = ToDo.objects.all()
+    return render(request, "test.html", {"todo_list": todo_list})
