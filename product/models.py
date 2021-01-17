c6a062a2b1c48902b0c0cb624962f687a9a92438
Daniil <d.palkin94@gmail.com>
2021-01-17 12:24:12 +0600

    added books.html and model of books

--- product/models.py
+++ product/models.py
@@ -8,3 +8,14 @@ class ToDo(models.Model):
     created_at = models.DateField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
+
+
+class Books(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=100)
+    description = models.CharField()
+    price = models.FloatField()
+    genre = models.CharField()
+    author = models.CharField()
+    year = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
